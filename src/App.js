e044adf0340d6c98929d381cd9776d16d00fdd1b
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-10-08 12:52:02 -0300

    Recomençando

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/layouts/Container";
+
+import styles from "./components/layouts/Container.modules.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Switch>
+        <Container>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+          <Route exact path="/newproject" element={<NewProject />}></Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
