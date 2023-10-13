8da21fb75a0c858556c9653565ea2efa50a8f391
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-10-13 14:23:24 -0300

    Projeto 13/10/2023
    - Criação do banco de dados e a utilização do useEffect

--- src/components/project/projectForm/ProjectForm.js
+++ src/components/project/projectForm/ProjectForm.js
@@ -4,23 +4,45 @@ import Input from "../../form/input/Inpu";
 import Select from "../../form/select/Select";
 import SubmitButton from "../../form/submitButton/SubmitButton";
 
-function ProjectForm({btnText}) {
+import { useEffect, useState } from "react";
+
+function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className="form">
-        <Input
-          type="text"
-          text="Nome do Projeto"
-          name={"name"}
-          placeholder="Insira o nome do projeto"
-        />
-        <Input
-          type="number"
-          text="Orçamento do Projeto"
-          name={"budget"}
-          placeholder="Insira o orçamento total"
-        />
-        <Select name={"category_id"} text={"Selecione a categoria"} />
-        <SubmitButton text={btnText} />
+      <Input
+        type="text"
+        text="Nome do Projeto"
+        name={"name"}
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do Projeto"
+        name={"budget"}
+        placeholder="Insira o orçamento total"
+      />
+      <Select
+        name={"category_id"}
+        text={"Selecione a categoria"}
+        options={categories}
+      />
+      <SubmitButton text={btnText} />
     </form>
   );
 }
