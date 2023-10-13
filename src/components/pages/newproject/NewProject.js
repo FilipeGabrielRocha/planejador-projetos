d9fe76bd4f93a136e5102480a548ae98da3d0095
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-10-13 11:13:27 -0300

    Projeto 13/10/2023
    - Criação dos  componentes NewProjects e ProjectForms

--- src/components/pages/newproject/NewProject.js
+++ src/components/pages/newproject/NewProject.js
@@ -1,5 +1,14 @@
+import ProjectForm from '../../project/projectForm/ProjectForm';
+import './NewProjectModule.css'
+
 function NewProject() {
-  return <h1>Novo Projeto</h1>;
+  return (
+    <div className="newproject_container">
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
+    </div>
+  )
 }
 
 export default NewProject;
