0d3d3eec8d7eb048cf1a97734e83c833170ebda3
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-10-25 06:09:49 -0300

    Projeto 25/10/2023
    - Criação da aba contato
    - Estilização da aba contato

--- src/components/layouts/footer/Footer.js
+++ src/components/layouts/footer/Footer.js
@@ -1,18 +1,38 @@
-import './footerModule.css'
+import { Link } from "react-router-dom";
 
-import {FaFacebookSquare, FaInstagramSquare, FaLinkedin} from 'react-icons/fa'
+import "./footerModule.css";
 
-function Footer(){
-    return(
-        <footer className='footer'>
-            <ul className='social_list'>
-                <li><FaFacebookSquare /></li>
-                <li><FaInstagramSquare /></li>
-                <li><FaLinkedin /></li>
-            </ul>
-            <p className='copy_right'><span>Gerenciador de Projetos</span> &copy; 2023</p>
-        </footer>
-    )
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <ul className="social_list">
+        <li>
+          <Link to={"/"}>
+            <FaFacebookSquare />
+          </Link>
+        </li>
+        <li>
+          <Link to={"https://www.instagram.com/_filiperochaa/"} target="_blank">
+            <FaInstagramSquare />
+          </Link>
+        </li>
+        <li>
+          <Link to={"https://www.linkedin.com/in/filipe-gabriel-rocha/"} target="_blank">
+            <FaLinkedin />
+          </Link>
+        </li>
+      </ul>
+      <p className="copy_right">
+        <span>Gerenciador de Projetos</span> &copy; 2023
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,5 +1,18 @@
+import './contactModule.css'
+
+import {TfiEmail} from 'react-icons/tfi'
+
 function Contact() {
-  return <h1>Contato</h1>;
+  return (
+    <div className='container_contato'>
+      <h2>Formas de entrar em contato</h2>
+      <div>
+        <p><span><TfiEmail /></span> filiperochaprogramador@gmail.com</p>
+        <p><span><TfiEmail /></span> filiperocha844@gmail.com</p>
+        <p><span><TfiEmail /></span> filipegrocha1430@gmail.com</p>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
